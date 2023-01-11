6aa047db38e6858ba91a28af7e21d86c9db57499
Fay <makkarennu14@gmail.com>
2023-01-11 10:33:29 +0700

    first commit

---
+++ hapus-siswa.php
@@ -0,0 +1,16 @@
+<?php
+
+include('koneksii.php');
+
+//get id
+$id = $_GET['id'];
+
+$query = "DELETE FROM tbl_siswa WHERE id_siswa = '$id'";
+
+if($connection->query($query)) {
+    header("location: index.php");
+} else {
+    echo "DATA GAGAL DIHAPUS!";
+}
+
+?>
